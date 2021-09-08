a6eff059a283140fa6e40e160dcdd18e1503d3b6
Pedro <ph.pfiorentin@gmail.com>
2021-09-08 16:22:10 +0200

    GPS tracker finished

---
+++ exercicio_gps_parte2_java/src/main/java/com/example/exercicio_gps_parte2_java/GPSTrackerActivity.java
@@ -0,0 +1,50 @@
+package com.example.exercicio_gps_parte2_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GPSTrackerActivity extends AppCompatActivity {
+    private TextView latitudeValue;
+    private TextView longitudeValue;
+
+    private Button gpsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gps_tracker_layout);
+        setTitle("Parte 2 - GPS");
+
+        latitudeValue = (TextView)findViewById(R.id.text_latitude);
+        longitudeValue = (TextView)findViewById(R.id.text_longitude);
+        gpsButton = (Button)findViewById(R.id.button_gps);
+
+        ActivityCompat.requestPermissions(GPSTrackerActivity.this, new
+                String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GPSTracker g = new GPSTracker(getApplicationContext());
+                Location l = g.getLocation();
+                if(l != null)
+                {
+                    double lat = l.getLatitude();
+                    double longi = l.getLongitude();
+
+                    latitudeValue.setText("Latitude: " + String.valueOf(lat));
+                    longitudeValue.setText("Longitude: " + String.valueOf(longi));
+                }
+            }
+        });
+    }
+}
--- exercicios_parte2_java/src/main/java/com/example/exercicios_parte2_java/SensorsActivity.java
+++ exercicios_parte2_java/src/main/java/com/example/exercicios_parte2_java/SensorsActivity.java
@@ -2,13 +2,67 @@ package com.example.exercicios_parte2_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SensorsActivity extends AppCompatActivity {
+public class SensorsActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private Sensor proximitySensor;
+
+    private TextView lightValue;
+    private TextView proximityValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensors_activity_layout);
+        setTitle("Parte 2");
+
+        lightValue = (TextView)findViewById(R.id.text_light);
+        proximityValue = (TextView)findViewById(R.id.text_proximity);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+
+        switch(sensor.getType()) {
+            case Sensor.TYPE_LIGHT:
+                float lux = event.values[0];
+                lightValue.setText(String.valueOf(lux));
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                float proximity = event.values[0];
+                proximityValue.setText(String.valueOf(proximity));
+                break;
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
     }
 }
